ccc5ecde982c32502a131f8065588e875cc51e72
Anish Dhondi <anishdb13@gmail.com>
2026-05-24 15:52:28 +0530

    MLOPS - Prerequisites

---
+++ MLOps - Prerequisite/Segment - 2/Naming Conventions/Good.py
@@ -0,0 +1,7 @@
+# Clear Code
+roll_no = [1, 2, 3, 4, 5]
+marks = [88, 99, 66, 100, 99]
+mean_marks = (88 + 99 + 66 + 100 + 99) / len(marks)
+for i in range(len(roll_no)):
+    print(roll_no[i], marks[i])
+print(mean_marks)
